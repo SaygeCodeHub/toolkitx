2a10908ea15255c6f2b72b6ff00067d635bd2635
Rucha Wajpe <62145318+ruchawajpe@users.noreply.github.com>
2023-07-19 12:52:19 +0530

    TXAD-145: Implement Get Master and Apply Leave API to Apply For Leave… (#77)
    * TXAD-145: Implement Get Master and Apply Leave API to Apply For Leave Screen.
    
    * TXAD-145: Implement Get Master and Apply Leave API to Apply For Leave Screen.
    
    * TXAD-145: Refactor Leaves Summary UI.

--- lib/utils/constants/string_constants.dart
+++ lib/utils/constants/string_constants.dart
@@ -65,6 +65,14 @@ abstract class StringConstants {
   static const String kAssignedByMe = 'assignedbyme'; //
   static const String kViewHistory = 'View History'; //
   static const String kLeaveDetails = 'Leave Details'; //
+  static const String kStartDateValidation = 'Please select start date!'; //
+  static const String kEndDateValidation = 'Please select end date!'; //
+  static const String kTypeValidation = 'Please select type!'; //
+  static const String kReasonValidation = 'Please enter reason!'; //
+  static const String kEmergencyContactValidation =
+      'Please enter emergency contact!'; //
+  static const String kSelect = 'Select'; //
+  static const String kAlert = 'Alert'; //
   static const String kCannotUnlinkPermit = 'Cannot unlink permit!';
   static const String kFiles = 'Files';
   static const String kOtpGenerated = 'OTP generated';
