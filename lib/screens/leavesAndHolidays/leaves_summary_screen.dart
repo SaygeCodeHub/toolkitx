dbc50dbc3303f8c7212687715005d77f8dffcc99
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2023-07-19 11:25:56 +0530

    TXAD-145: Refactor Leaves Summary UI.

--- lib/screens/leavesAndHolidays/leaves_summary_screen.dart
+++ lib/screens/leavesAndHolidays/leaves_summary_screen.dart
@@ -9,6 +9,7 @@ import 'package:toolkit/widgets/error_section.dart';
 import 'package:toolkit/widgets/generic_app_bar.dart';
 import '../../blocs/leavesAndHolidays/leaves_and_holidays_events.dart';
 import '../../configs/app_color.dart';
+import '../../configs/app_dimensions.dart';
 import '../../configs/app_spacing.dart';
 import '../../widgets/custom_card.dart';
 import 'widgtes/leaves_summary_subtitle.dart';
@@ -35,36 +36,41 @@ class LeavesSummaryScreen extends StatelessWidget {
                     left: leftRightMargin,
                     right: leftRightMargin,
                     top: xxTinierSpacing),
-                child: ListView.separated(
-                    physics: const BouncingScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: state.fetchLeavesSummaryModel.data.length,
-                    itemBuilder: (context, index) {
-                      return CustomCard(
-                          child: Padding(
-                              padding:
-                                  const EdgeInsets.only(top: tinierSpacing),
-                              child: ListTile(
-                                  title: Text(
-                                      state.fetchLeavesSummaryModel.data[index]
-                                          .name,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .small
-                                          .copyWith(
-                                              fontWeight: FontWeight.bold,
-                                              color: AppColor.black)),
-                                  subtitle: LeavesSummarySubtitle(
-                                      data: state.fetchLeavesSummaryModel
-                                          .data[index]))));
-                    },
-                    separatorBuilder: (context, index) {
-                      return const SizedBox(height: tinierSpacing);
-                    })),
+                child: GridView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: state.fetchLeavesSummaryModel.data.length,
+                  itemBuilder: (context, index) {
+                    return CustomCard(
+                        child: Padding(
+                            padding: const EdgeInsets.only(top: tinierSpacing),
+                            child: ListTile(
+                                title: Text(
+                                    state.fetchLeavesSummaryModel.data[index]
+                                        .name,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .small
+                                        .copyWith(
+                                            fontWeight: FontWeight.bold,
+                                            color: AppColor.black)),
+                                subtitle: LeavesSummarySubtitle(
+                                    data: state.fetchLeavesSummaryModel
+                                        .data[index]))));
+                  },
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisExtent: kGridViewMainAxisExtent,
+                      crossAxisSpacing: tinierSpacing,
+                      mainAxisSpacing: tinierSpacing),
+                )),
           );
         } else if (state is LeavesSummaryNotFetched) {
           return GenericReloadButton(
-              onPressed: () {}, textValue: StringConstants.kReload);
+              onPressed: () {
+                context.read<LeavesAndHolidaysBloc>().add(FetchLeavesSummary());
+              },
+              textValue: StringConstants.kReload);
         } else {
           return const SizedBox();
         }
