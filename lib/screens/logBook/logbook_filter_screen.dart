213b8c87edbf6342859fa475404149e89f5052be
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2023-07-28 13:38:05 +0530

    TXAD-27: Create logbook filter screen file.

---
+++ lib/screens/logBook/logbook_filter_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LogBookFilterScreen extends StatelessWidget {
+  const LogBookFilterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
