f39db7447b969b1b48d3aedc3d481fdb4f74edfa
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2023-09-04 16:57:35 +0530

    TXAD-36: Add WorkOrder Tab five details file.

---
+++ lib/screens/workorder/widgets/workorder_tab_five_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/models/workorder/fetch_workorder_details_model.dart';
+
+class WorkOrderTabFiveDetails extends StatelessWidget {
+  final WorkOrderDetailsData data;
+  final int tabIndex;
+
+  const WorkOrderTabFiveDetails(
+      {Key? key, required this.data, required this.tabIndex})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
