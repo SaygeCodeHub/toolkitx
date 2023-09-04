22084499241a2ddb868c90a9d9539ea6f7346325
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2023-09-04 17:21:13 +0530

    TXAD-36: Implement WorkOrder Details Tab Five Data.

--- lib/screens/workorder/widgets/workorder_tab_five_details.dart
+++ lib/screens/workorder/widgets/workorder_tab_five_details.dart
@@ -1,17 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:toolkit/configs/app_theme.dart';
 
+import '../../../configs/app_color.dart';
+import '../../../configs/app_spacing.dart';
 import '../../../data/models/workorder/fetch_workorder_details_model.dart';
+import '../../../utils/constants/string_constants.dart';
+import '../../../widgets/custom_card.dart';
+import 'workorder_view_network_image.dart';
 
 class WorkOrderTabFiveDetails extends StatelessWidget {
   final WorkOrderDetailsData data;
   final int tabIndex;
+  final String clientId;
 
   const WorkOrderTabFiveDetails(
-      {Key? key, required this.data, required this.tabIndex})
+      {Key? key,
+      required this.data,
+      required this.tabIndex,
+      required this.clientId})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return (data.comments.isEmpty)
+        ? Center(
+            child: Text(StringConstants.kNoComment,
+                style: Theme.of(context).textTheme.small.copyWith(
+                    fontWeight: FontWeight.w700, color: AppColor.mediumBlack)))
+        : ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            itemCount: data.comments.length,
+            itemBuilder: (context, index) {
+              return CustomCard(
+                child: ListTile(
+                  contentPadding: const EdgeInsets.only(
+                      left: tinierSpacing,
+                      right: tinierSpacing,
+                      top: tiniestSpacing,
+                      bottom: tiniestSpacing),
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(data.comments[index].ownername,
+                          style: Theme.of(context).textTheme.small.copyWith(
+                              fontWeight: FontWeight.w700,
+                              color: AppColor.mediumBlack)),
+                      Text(data.comments[index].created,
+                          style: Theme.of(context)
+                              .textTheme
+                              .xSmall
+                              .copyWith(fontWeight: FontWeight.w500))
+                    ],
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: xxxTinierSpacing),
+                      Text(data.comments[index].comments,
+                          style: Theme.of(context).textTheme.xSmall),
+                      const SizedBox(height: xxTinierSpacing),
+                      Visibility(
+                          visible: data.comments[index].files != '',
+                          child: WorkOrderViewNetworkImage(
+                              comment: data.comments[index],
+                              clientId: clientId)),
+                    ],
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return const SizedBox(height: xxTinierSpacing);
+            },
+          );
   }
 }
