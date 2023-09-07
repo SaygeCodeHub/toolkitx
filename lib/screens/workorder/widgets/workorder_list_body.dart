10a6c0c912c9d2dc929becab6d27cea50112c62a
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2023-09-07 22:11:13 +0530

    TXAD-160: Fix WorkForce List Screen issue.

--- lib/screens/workorder/widgets/workorder_list_body.dart
+++ lib/screens/workorder/widgets/workorder_list_body.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:toolkit/screens/workorder/widgets/workorder_list_card.dart';
+import 'package:toolkit/utils/database_utils.dart';
 
 import '../../../blocs/workorder/workorder_bloc.dart';
 import '../../../blocs/workorder/workorder_events.dart';
@@ -8,7 +11,6 @@ import '../../../blocs/workorder/workorder_states.dart';
 import '../../../configs/app_dimensions.dart';
 import '../../../configs/app_spacing.dart';
 import '../../../utils/constants/string_constants.dart';
-import '../../../utils/database_utils.dart';
 import '../../../widgets/custom_snackbar.dart';
 import '../../../widgets/generic_no_records_text.dart';
 import '../workorder_list_screen.dart';
@@ -18,6 +20,8 @@ class WorkOrderListBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    context.read<WorkOrderBloc>().hasReachedMax = false;
+    context.read<WorkOrderBloc>().data.clear();
     return BlocConsumer<WorkOrderBloc, WorkOrderStates>(
         buildWhen: (previousState, currentState) =>
             ((currentState is WorkOrdersFetched &&
@@ -26,9 +30,10 @@ class WorkOrderListBody extends StatelessWidget {
                     WorkOrderListScreen.pageNo == 1)),
         listener: (context, state) {
           if (state is WorkOrdersFetched) {
-            if (state.fetchWorkOrdersModel.status == 204 &&
-                state.filterMap.isEmpty) {
-              context.read<WorkOrderBloc>().hasReachedMax = true;
+            if (state.fetchWorkOrdersModel.status == 204) {
+              log("yaha kisne aane ko bola bsdk========>");
+              // context.read<WorkOrderBloc>().hasReachedMax = true;
+              WorkOrderListScreen.pageNo = 1;
               showCustomSnackBar(context, StringConstants.kAllDataLoaded, '');
             }
           }
@@ -46,13 +51,16 @@ class WorkOrderListBody extends StatelessWidget {
                   child: ListView.separated(
                       physics: const BouncingScrollPhysics(),
                       shrinkWrap: true,
-                      itemCount: state.hasReachedMax
-                          ? state.data.length
-                          : state.data.length + 1,
+                      itemCount:
+                          context.read<WorkOrderBloc>().hasReachedMax == true
+                              ? state.data.length
+                              : state.data.length + 1,
                       itemBuilder: (context, index) {
                         if (index < state.data.length) {
                           return WorkOrderListCard(data: state.data[index]);
                         } else if (!state.hasReachedMax) {
+                          log("i am here======>");
+                          log("istate.hasReachedMax======>${state.hasReachedMax}");
                           WorkOrderListScreen.pageNo++;
                           context.read<WorkOrderBloc>().add(FetchWorkOrders(
                               pageNo: WorkOrderListScreen.pageNo,
@@ -73,18 +81,18 @@ class WorkOrderListBody extends StatelessWidget {
                       separatorBuilder: (context, index) {
                         return const SizedBox(height: xxTinySpacing);
                       }));
-            } else {
-              if (state.fetchWorkOrdersModel.status == 204) {
-                if (state.filterMap.isEmpty) {
-                  return const NoRecordsText(
-                      text: StringConstants.kNoRecordsFilter);
-                } else {
-                  return NoRecordsText(
-                      text: DatabaseUtil.getText('no_records_found'));
-                }
+            } else if (state.fetchWorkOrdersModel.status == 204 &&
+                context.read<WorkOrderBloc>().data.isEmpty) {
+              log("are you mf stupid========>");
+              if (state.filterMap.isEmpty) {
+                return const NoRecordsText(
+                    text: StringConstants.kNoRecordsFilter);
               } else {
-                return const SizedBox.shrink();
+                return NoRecordsText(
+                    text: DatabaseUtil.getText('no_records_found'));
               }
+            } else {
+              return const SizedBox.shrink();
             }
           } else {
             return const SizedBox.shrink();
