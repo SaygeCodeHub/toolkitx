6e2c8708d703963ae79ceda33ab2b2c7b740524c
Rucha Wajpe <62145318+ruchawajpe@users.noreply.github.com>
2023-09-16 14:26:09 +0530

    TXAD-168: Implement Delete Document and Delete Item API. (#121)
    * TXAD-168: Implement Delete Document and Delete Item API.
    
    * TXAD-168: Implement Delete Document and Delete Item API.
    
    * TXAD-168: Fix code formatting issue
    
    * TXAD-168: Remove map from WorkOrder Tab Three Items Tab.
    
    * TXAD-168: Remove map from WorkOrder Tab Three Items Tab.

---
+++ lib/screens/workorder/widgets/workorder_item_tab_delete_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../blocs/workorder/workOrderTabsDetails/workorder_tab_details_bloc.dart';
+import '../../../blocs/workorder/workOrderTabsDetails/workorder_tab_details_events.dart';
+import '../../../configs/app_dimensions.dart';
+import '../../../utils/database_utils.dart';
+import '../../../widgets/android_pop_up.dart';
+import '../../../widgets/custom_icon_button.dart';
+
+class WorkOrderItemTabDeleteButton extends StatelessWidget {
+  final String itemId;
+
+  const WorkOrderItemTabDeleteButton({Key? key, required this.itemId})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIconButton(
+        icon: Icons.delete,
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AndroidPopUp(
+                    titleValue: DatabaseUtil.getText('DeleteRecord'),
+                    contentValue: '',
+                    onPrimarybtn: () {
+                      context
+                          .read<WorkOrderTabDetailsBloc>()
+                          .add(WorkOrderItemTabDeleteItem(itemId: itemId));
+                      Navigator.pop(context);
+                    });
+              });
+        },
+        size: kEditAndDeleteIconTogether);
+  }
+}
