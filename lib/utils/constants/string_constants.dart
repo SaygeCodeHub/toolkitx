d8cf9a94510c6d58d81a1d1fd4b7771dffe02b0b
nikhilagashe <nikhilagashe31@gmail.com>
2023-10-03 17:14:54 +0530

    TXAD-215:Implement Assign Workforce API and Pagination in Loto screen

--- lib/utils/constants/string_constants.dart
+++ lib/utils/constants/string_constants.dart
@@ -296,6 +296,8 @@ abstract class StringConstants {
   static const String kWarning = 'Warning';
   static const String kCapture = 'Capture';
   static const String kQRError = 'Invalid Location';
+  static const String kLotoStart = 'LOTO Start';
+  static const String kLotoStarted = "Loto Started";
   static const String kLocationDocuments = "Location Documents";
   static const String kLoginSuccess = 'Logged in Successfully';
   static const String kConfirmAssetWithQR = "Confirm Asset with QRCode";
@@ -304,4 +306,6 @@ abstract class StringConstants {
   static const String kUnauthorizedError = 'oops we got unknown error';
   static const String kYouHaveCoursesToComplete =
       'You have 1 courses to complete this certificate, keep it up!';
+  static const String kCarefulAndOrganized =
+      '"A careful and organized approach to LOTO is very important. Before applying LOTO, please down the machine or equipment and isolate them for any source of energy."';
 }
