5bd1f556efb73a5c71f07f255edf5d79f73c11ce
nikhilagashe <117993439+nikhilagashe@users.noreply.github.com>
2023-10-07 14:24:11 +0530

    TXAD-161:Implement Get Certificate API in Certificate screen.
    * TXAD-192:Implement Get Certificate API in Certificate screen
    
    * TXAD-161:Implement Get Certificate API in Certificate screen
    
    * TXAD-161:Implement Get Certificate API in Certificate screen
    
    * TXAD-161:Implement Get Certificate API in Certificate screen
    
    ---------
    
    Co-authored-by: Rucha Wajpe <rucha.wajpe2233@gmail.com>

---
+++ lib/screens/certificates/widgets/certificate_list_card.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:toolkit/configs/app_theme.dart';
+import '../../../blocs/certificates/cerficatesList/certificate_list_bloc.dart';
+import '../../../configs/app_color.dart';
+import '../../../configs/app_dimensions.dart';
+import '../../../configs/app_spacing.dart';
+import '../../../data/models/certificates/certificate_list_model.dart';
+import '../../../utils/constants/string_constants.dart';
+import '../../../widgets/custom_card.dart';
+import '../../../widgets/text_button.dart';
+import '../feedback_certificate_screen.dart';
+import '../get_certificate_details_screen.dart';
+import '../get_course_certificate_screen.dart';
+import '../upload_certificate_screen.dart';
+
+class CertificateListCard extends StatelessWidget {
+  const CertificateListCard({super.key, required this.data});
+
+  final CertificateListDatum data;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomCard(
+        elevation: 2,
+        child: Padding(
+            padding: const EdgeInsets.only(top: tinierSpacing),
+            child: Column(children: [
+              ListTile(
+                onTap: () {},
+                title: Text(data.name,
+                    style: Theme.of(context).textTheme.small.copyWith(
+                        fontWeight: FontWeight.w500, color: AppColor.black)),
+                subtitle: const Text(StringConstants.kNotAvailable),
+                trailing: Image.asset('assets/icons/certificate.png',
+                    height: kImageHeight, width: kImageWidth),
+              ),
+              const Divider(
+                color: AppColor.lightestGrey,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                Expanded(
+                    child: CustomTextButton(
+                        onPressed: () {
+                          Map certificateMap = {
+                            "title": data.name,
+                            "id": data.id
+                          };
+
+                          if (context
+                                  .read<CertificateListBloc>()
+                                  .fetchCertificateDetailsModel
+                                  .data
+                                  ?.status ==
+                              "1") {
+                            Navigator.pushNamed(
+                                context, GetCertificateDetailsScreen.routeName,
+                                arguments: certificateMap);
+                          } else {
+                            Navigator.pushNamed(
+                                context, UploadCertificateScreen.routeName,
+                                arguments: certificateMap);
+                          }
+                        },
+                        textValue: StringConstants.kUpload)),
+                Expanded(
+                    child: CustomTextButton(
+                        onPressed: () {},
+                        textValue: StringConstants.kDownload)),
+                Expanded(
+                    child: CustomTextButton(
+                        onPressed: () {
+                          String certificateId = data.id;
+                          Navigator.pushNamed(
+                              context, GetCourseCertificateScreen.routeName,
+                              arguments: certificateId);
+                        },
+                        textValue: StringConstants.kStartCourse)),
+                Expanded(
+                    child: CustomTextButton(
+                        onPressed: () {
+                          Map certificateMap = {
+                            "title": data.name,
+                            "id": data.id
+                          };
+                          Navigator.pushNamed(
+                              context, FeedbackCertificateScreen.routeName,
+                              arguments: certificateMap);
+                        },
+                        textValue: StringConstants.kFeedback))
+              ])
+            ])));
+  }
+}
