eb84f6ba8d89addd7dda2ad535bd847502f0c4ed
Aditi-005 <111351516+Aditi-005@users.noreply.github.com>
2023-10-14 12:25:05 +0530

    TXAD-243: Documents Details Screen Tab 5 and 6.
    * TXAD-185: Documents Details Screen Tab 5 and 6.
    
    * TXAD-243: Requested changes.

--- lib/screens/documents/widgets/documents_details_files.dart
+++ lib/screens/documents/widgets/documents_details_files.dart
@@ -10,15 +10,15 @@ import '../../../utils/constants/api_constants.dart';
 import '../../../utils/constants/string_constants.dart';
 import '../../../utils/documents_util.dart';
 import '../../../utils/generic_alphanumeric_generator_util.dart';
-import '../../../utils/incident_view_image_util.dart';
 import '../../../widgets/custom_card.dart';
 import 'document_files_menu.dart';
 
 class DocumentDetailsFiles extends StatelessWidget {
   final DocumentDetailsModel documentDetailsModel;
-  static String clientId = '';
+  final String clientId;
 
-  const DocumentDetailsFiles({super.key, required this.documentDetailsModel});
+  const DocumentDetailsFiles(
+      {super.key, required this.documentDetailsModel, required this.clientId});
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +39,7 @@ class DocumentDetailsFiles extends StatelessWidget {
                       child: ListTile(
                           onTap: () {
                             launchUrlString(
-                                '${ApiConstants.viewDocBaseUrl}${ViewImageUtil.viewImageList(documentDetailsModel.data.fileList[index].filename)[index]}&code=${RandomValueGeneratorUtil.generateRandomValue(clientId)}',
+                                '${ApiConstants.viewDocBaseUrl}${documentDetailsModel.data.fileList[index].filename[index]}&code=${RandomValueGeneratorUtil.generateRandomValue(clientId)}',
                                 mode: LaunchMode.externalApplication);
                           },
                           title: Column(
