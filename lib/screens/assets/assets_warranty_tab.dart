cf91f7fc9fcfc1e25044ed7bfe2e811559af2335
nikhilagashe <117993439+nikhilagashe@users.noreply.github.com>
2023-10-16 12:44:16 +0530

    TXAD-248:Implement Assets Tab 2 in Assets screen

---
+++ lib/screens/assets/assets_warranty_tab.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:toolkit/configs/app_spacing.dart';
+import 'package:toolkit/configs/app_theme.dart';
+import 'package:toolkit/data/models/assets/assets_details_model.dart';
+import 'package:toolkit/utils/constants/string_constants.dart';
+
+import '../../configs/app_color.dart';
+
+class AssetsWarrantyTab extends StatelessWidget {
+  const AssetsWarrantyTab({super.key, required this.data});
+
+  final Data data;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(StringConstants.kWarranty,
+              style: Theme.of(context)
+                  .textTheme
+                  .medium
+                  .copyWith(fontWeight: FontWeight.w500, color: AppColor.grey)),
+          const SizedBox(height: smallestSpacing),
+          Text(StringConstants.kWarrantyStart,
+              style: Theme.of(context).textTheme.smallTextBlack),
+          const SizedBox(height: tiniestSpacing),
+          Text(data.warrantystart,
+              style: Theme.of(context).textTheme.smallTextGrey),
+          const SizedBox(height: xxxSmallestSpacing),
+          Text(StringConstants.kWarrantyEnd,
+              style: Theme.of(context).textTheme.smallTextBlack),
+          const SizedBox(height: tiniestSpacing),
+          Text(data.warrantyend,
+              style: Theme.of(context).textTheme.smallTextGrey),
+        ]));
+  }
+}
