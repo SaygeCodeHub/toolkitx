f18d8aebfd0628b10f0920ba8972a3a35e5cde06
Rucha Wajpe <62145318+ruchawajpe@users.noreply.github.com>
2023-11-04 17:26:27 +0530

    TXAD-225: Implement Delete WorkForce API to WorkOrder WorkForce Tab.
    * TXAD-225: Add Bloc files for Delete WorkOrder WorkForce.
    
    * TXAD-225: Implement Delete WorkForce API to WorkOrder WorkForce Tab.

--- lib/screens/workorder/widgets/workorder_tab_two_details.dart
+++ lib/screens/workorder/widgets/workorder_tab_two_details.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:toolkit/configs/app_theme.dart';
+import 'package:toolkit/utils/constants/string_constants.dart';
 import 'package:toolkit/utils/database_utils.dart';
+import 'package:toolkit/widgets/android_pop_up.dart';
 
+import '../../../blocs/workorder/workOrderTabsDetails/workorder_tab_details_bloc.dart';
+import '../../../blocs/workorder/workOrderTabsDetails/workorder_tab_details_events.dart';
 import '../../../configs/app_color.dart';
 import '../../../configs/app_dimensions.dart';
 import '../../../configs/app_spacing.dart';
@@ -22,93 +27,119 @@ class WorkOrderTabTwoDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        physics: const BouncingScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: data.workforce.length,
-        itemBuilder: (context, index) {
-          return CustomCard(
-            child: ListTile(
-              contentPadding: const EdgeInsets.all(xxTinierSpacing),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(data.workforce[index].name,
-                      style: Theme.of(context).textTheme.small.copyWith(
-                          color: AppColor.black, fontWeight: FontWeight.w600)),
-                  const Spacer(),
-                  CustomIconButton(
-                      icon: Icons.delete,
-                      onPressed: () {},
-                      size: kEditAndDeleteIconTogether),
-                  const SizedBox(width: xxxTinierSpacing),
-                  CustomIconButton(
-                      icon: Icons.edit,
-                      onPressed: () {
-                        WorkOrderEditWorkForceScreen
-                                .editWorkOrderWorkForceMap['plannedhrs'] =
-                            data.workforce[index].plannedhrs.toString();
-                        WorkOrderEditWorkForceScreen
-                                .editWorkOrderWorkForceMap['workForceName'] =
-                            data.workforce[index].name;
-                        WorkOrderEditWorkForceScreen
-                            .editWorkOrderWorkForceMap['workorderId'] = data.id;
-                        WorkOrderEditWorkForceScreen
-                                .editWorkOrderWorkForceMap['workForceId'] =
-                            data.workforce[index].workforceid;
-                        Navigator.pushNamed(
-                            context, WorkOrderEditWorkForceScreen.routeName);
-                      },
-                      size: kEditAndDeleteIconTogether)
-                ],
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(data.workforce[index].jobTitle,
-                      style: Theme.of(context).textTheme.xSmall.copyWith()),
-                  const SizedBox(height: tinierSpacing),
-                  Row(children: [
-                    Text(DatabaseUtil.getText('PlannedWorkingHours'),
-                        style: Theme.of(context)
-                            .textTheme
-                            .xSmall
-                            .copyWith(color: AppColor.grey)),
-                    const SizedBox(width: tiniestSpacing),
-                    Text(data.workforce[index].plannedhrs.toString(),
-                        style: Theme.of(context)
-                            .textTheme
-                            .xSmall
-                            .copyWith(color: AppColor.grey))
-                  ]),
-                  const SizedBox(height: tinierSpacing),
-                  Row(
-                    children: [
-                      Text(DatabaseUtil.getText('ActualWorkingHours'),
+    return Visibility(
+      visible: data.workforce.isNotEmpty,
+      replacement: Center(
+        child: Text(StringConstants.kNoWorkForce,
+            style: Theme.of(context).textTheme.medium),
+      ),
+      child: ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: data.workforce.length,
+          itemBuilder: (context, index) {
+            return CustomCard(
+              child: ListTile(
+                contentPadding: const EdgeInsets.all(xxTinierSpacing),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(data.workforce[index].name,
+                        style: Theme.of(context).textTheme.small.copyWith(
+                            color: AppColor.black,
+                            fontWeight: FontWeight.w600)),
+                    const Spacer(),
+                    CustomIconButton(
+                        icon: Icons.delete,
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AndroidPopUp(
+                                    titleValue:
+                                        DatabaseUtil.getText('DeleteRecord'),
+                                    contentValue: '',
+                                    onPrimaryButton: () {
+                                      context
+                                          .read<WorkOrderTabDetailsBloc>()
+                                          .add(DeleteWorkOrderWorkForce(
+                                              workForceId:
+                                                  data.workforce[index].id));
+                                      Navigator.pop(context);
+                                    });
+                              });
+                        },
+                        size: kEditAndDeleteIconTogether),
+                    const SizedBox(width: xxxTinierSpacing),
+                    CustomIconButton(
+                        icon: Icons.edit,
+                        onPressed: () {
+                          WorkOrderEditWorkForceScreen
+                                  .editWorkOrderWorkForceMap['plannedhrs'] =
+                              data.workforce[index].plannedhrs.toString();
+                          WorkOrderEditWorkForceScreen
+                                  .editWorkOrderWorkForceMap['workForceName'] =
+                              data.workforce[index].name;
+                          WorkOrderEditWorkForceScreen
+                                  .editWorkOrderWorkForceMap['workorderId'] =
+                              data.id;
+                          WorkOrderEditWorkForceScreen
+                                  .editWorkOrderWorkForceMap['workForceId'] =
+                              data.workforce[index].workforceid;
+                          Navigator.pushNamed(
+                              context, WorkOrderEditWorkForceScreen.routeName);
+                        },
+                        size: kEditAndDeleteIconTogether)
+                  ],
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(data.workforce[index].jobTitle,
+                        style: Theme.of(context).textTheme.xSmall.copyWith()),
+                    const SizedBox(height: tinierSpacing),
+                    Row(children: [
+                      Text(DatabaseUtil.getText('PlannedWorkingHours'),
                           style: Theme.of(context)
                               .textTheme
                               .xSmall
                               .copyWith(color: AppColor.grey)),
                       const SizedBox(width: tiniestSpacing),
-                      Text(data.workforce[index].actualhrs.toString(),
+                      Text(data.workforce[index].plannedhrs.toString(),
                           style: Theme.of(context)
                               .textTheme
                               .xSmall
                               .copyWith(color: AppColor.grey))
-                    ],
-                  ),
-                  const SizedBox(height: tinierSpacing),
-                  StatusTag(tags: [
-                    WorkOrderTabTwoStatusUtil().workOrderStatusTag(
-                        data.workforce[index].certificatecode)
-                  ])
-                ],
+                    ]),
+                    const SizedBox(height: tinierSpacing),
+                    Row(
+                      children: [
+                        Text(DatabaseUtil.getText('ActualWorkingHours'),
+                            style: Theme.of(context)
+                                .textTheme
+                                .xSmall
+                                .copyWith(color: AppColor.grey)),
+                        const SizedBox(width: tiniestSpacing),
+                        Text(data.workforce[index].actualhrs.toString(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .xSmall
+                                .copyWith(color: AppColor.grey))
+                      ],
+                    ),
+                    const SizedBox(height: tinierSpacing),
+                    StatusTag(tags: [
+                      WorkOrderTabTwoStatusUtil().workOrderStatusTag(
+                          data.workforce[index].certificatecode)
+                    ])
+                  ],
+                ),
               ),
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return const SizedBox(height: xxTinySpacing);
-        });
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: xxTinySpacing);
+          }),
+    );
   }
 }
