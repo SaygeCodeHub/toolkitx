5ee53fea23bd6981d2194109acb0b293bdf9a612
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2023-11-20 11:25:50 +0530

    TXAD-298: Implement Get Location API to Location Details Tab One screen.

--- lib/repositories/location/location_repository.dart
+++ lib/repositories/location/location_repository.dart
@@ -1,6 +1,10 @@
+import '../../data/models/location/fetch_location_details_model.dart';
 import '../../data/models/location/fetch_locations_model.dart';
 
 abstract class LocationRepository {
   Future<FetchLocationsModel> fetchLocations(
       int pageNo, String hashCode, String filter);
+
+  Future<FetchLocationDetailsModel> fetchLocationDetails(
+      String locationId, String hashCode);
 }
---
+++ lib/screens/location/location_details_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:toolkit/configs/app_theme.dart';
+import 'package:toolkit/utils/constants/string_constants.dart';
+import 'package:toolkit/widgets/error_section.dart';
+
+import '../../blocs/location/location_bloc.dart';
+import '../../blocs/location/location_event.dart';
+import '../../blocs/location/location_state.dart';
+import '../../configs/app_color.dart';
+import '../../configs/app_dimensions.dart';
+import '../../configs/app_spacing.dart';
+import '../../utils/location_tabs_util.dart';
+import '../../widgets/custom_tabbar_view.dart';
+import '../../widgets/generic_app_bar.dart';
+import 'widgets/location_details_tab_one.dart';
+
+class LocationDetailsScreen extends StatelessWidget {
+  final String expenseId;
+  static const routeName = 'LocationDetailsScreen';
+
+  const LocationDetailsScreen({Key? key, required this.expenseId})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    context
+        .read<LocationBloc>()
+        .add(FetchLocationDetails(locationId: expenseId, selectedTabIndex: 1));
+    return Scaffold(
+        appBar: const GenericAppBar(title: StringConstants.kLocationDetails),
+        body: BlocBuilder<LocationBloc, LocationState>(
+          buildWhen: (previousState, currentState) =>
+              currentState is FetchingLocationDetails ||
+              currentState is LocationDetailsFetched ||
+              currentState is LocationDetailsNotFetched,
+          builder: (context, state) {
+            if (state is FetchingLocationDetails) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is LocationDetailsFetched) {
+              return Padding(
+                  padding: const EdgeInsets.only(
+                      left: leftRightMargin,
+                      right: leftRightMargin,
+                      top: xxTinierSpacing),
+                  child: Column(children: [
+                    Card(
+                        color: AppColor.white,
+                        elevation: kCardElevation,
+                        child: ListTile(
+                            title: Padding(
+                                padding:
+                                    const EdgeInsets.only(top: xxTinierSpacing),
+                                child: Text(
+                                    state.fetchLocationDetailsModel.data.name,
+                                    style:
+                                        Theme.of(context).textTheme.medium)))),
+                    const SizedBox(height: xxTinierSpacing),
+                    const Divider(
+                        height: kDividerHeight, thickness: kDividerWidth),
+                    CustomTabBarView(
+                        lengthOfTabs: state.selectedTabIndex,
+                        tabBarViewIcons: LocationTabsUtil().tabBarViewIcons,
+                        initialIndex: 1,
+                        tabBarViewWidgets: [
+                          LocationDetailsTabOne(
+                              data: state.fetchLocationDetailsModel.data,
+                              selectedTabIndex: 1)
+                        ])
+                  ]));
+            } else if (state is LocationDetailsNotFetched) {
+              return Column(
+                children: [
+                  GenericReloadButton(
+                      onPressed: () {
+                        context.read<LocationBloc>().add(FetchLocationDetails(
+                            locationId: expenseId, selectedTabIndex: 1));
+                      },
+                      textValue: StringConstants.kReload),
+                  const SizedBox(height: xxTinierSpacing),
+                  Text(state.detailsNotFetched,
+                      style: Theme.of(context).textTheme.medium)
+                ],
+              );
+            } else {
+              return const SizedBox.shrink();
+            }
+          },
+        ));
+  }
+}
