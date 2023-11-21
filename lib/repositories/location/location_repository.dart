5093b71dd1d1dd9d4fc8b917a1f243026801280b
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2023-11-21 10:52:49 +0530

    TXAD-302: Implement Fetch Location WorkOrders API to Location Details WorkOrders Tab.

--- lib/repositories/location/location_repository.dart
+++ lib/repositories/location/location_repository.dart
@@ -1,18 +1,22 @@
 import '../../data/models/location/fetch_location_details_model.dart';
 import '../../data/models/location/fetch_location_loto_model.dart';
 import '../../data/models/location/fetch_location_permits_model.dart';
+import '../../data/models/location/fetch_location_workorders_model.dart';
 import '../../data/models/location/fetch_locations_model.dart';
 
 abstract class LocationRepository {
-  Future<FetchLocationsModel> fetchLocations(
-      int pageNo, String hashCode, String filter);
+  Future<FetchLocationsModel> fetchLocations(int pageNo, String hashCode,
+      String filter);
 
-  Future<FetchLocationDetailsModel> fetchLocationDetails(
-      String locationId, String hashCode);
+  Future<FetchLocationDetailsModel> fetchLocationDetails(String locationId,
+      String hashCode);
 
-  Future<FetchLocationPermitsModel> fetchLocationPermits(
-      int pageNo, String hashCode, String filter, String locationId);
+  Future<FetchLocationPermitsModel> fetchLocationPermits(int pageNo,
+      String hashCode, String filter, String locationId);
 
   Future<FetchLocationLoToModel> fetchLocationLoTo(int pageNo, String hashCode,
       String userId, String filter, String locationId);
+
+  Future<FetchLocationWorkOrdersModel> fetchLocationWorkOrders(int pageNo,
+      String hashCode, String filter, String locationId);
 }
