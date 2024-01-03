a965593f3fef655933503301197f505dbd0e13b5
nikhilagashe <nikhilagashe31@gmail.com>
2024-01-03 17:22:58 +0530

    TXAD-321: Change Select Permit text to Link Permit on Permit List.

--- lib/utils/constants/string_constants.dart
+++ lib/utils/constants/string_constants.dart
@@ -310,7 +310,7 @@ abstract class StringConstants {
   static const String kPagesInside = 'Pages inside';
   static const String kQuestionsInside = 'Questions inside';
   static const String kNameValidation = 'Please enter name';
-  static const String kSelectPermits = 'Select Permits';
+  static const String kSelectPermits = 'Link Permits';
   static const String kAddLinkToPermit = 'Add Link To Permit';
   static const String kSignInHere = 'Sign in here';
   static const String kGettingStarted = "Getting Started";
