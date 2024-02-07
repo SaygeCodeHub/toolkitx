d2b13215d261e863bcde690ee8a2a5c5268a2fcc
Jai Raut <jairaut456@gmail.com>
2024-02-07 18:57:39 +0530

    TXAD-373:Implement CheckBox on a LeaveAndHolidaysScreen and LeaveAndHolidaysBody screen

--- lib/screens/leavesAndHolidays/timesheet_checkin_screen.dart
+++ lib/screens/leavesAndHolidays/timesheet_checkin_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:toolkit/screens/leavesAndHolidays/add_and_edit_timesheet_screen.dart';
