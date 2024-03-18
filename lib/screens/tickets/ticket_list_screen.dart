7f4e54d41c01a0b4af9f6cf9cae3a199433f32d4
nikhilagashe <117993439+nikhilagashe@users.noreply.github.com>
2024-03-18 14:35:35 +0530

    TXAD-397 : Implement Get API in Tickets.

---
+++ lib/screens/tickets/ticket_list_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:toolkit/blocs/tickets/tickets_bloc.dart';
+import 'package:toolkit/screens/tickets/widgets/ticket_list_body.dart';
+import 'package:toolkit/utils/constants/string_constants.dart';
+import 'package:toolkit/utils/database_utils.dart';
+import 'package:toolkit/widgets/generic_app_bar.dart';
+import '../../configs/app_spacing.dart';
+import '../../widgets/custom_icon_button_row.dart';
+
+class TicketListScreen extends StatelessWidget {
+  static const routeName = 'TicketListScreen';
+  static int pageNo = 1;
+
+  const TicketListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    context.read<TicketsBloc>().add(FetchTickets(pageNo: pageNo));
+    return Scaffold(
+        appBar: GenericAppBar(title: DatabaseUtil.getText('Ticket')),
+        body: Padding(
+            padding: const EdgeInsets.only(
+                left: leftRightMargin,
+                right: leftRightMargin,
+                top: xxTinierSpacing,
+                bottom: xxTinierSpacing),
+            child: BlocBuilder<TicketsBloc, TicketsStates>(
+                builder: (context, state) {
+              if (state is TicketsFetching) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (state is TicketsFetched) {
+                var data = state.fetchTicketsModel.data;
+                return Column(children: [
+                  CustomIconButtonRow(
+                      primaryOnPress: () {},
+                      secondaryVisible: false,
+                      isEnabled: true,
+                      secondaryOnPress: () {},
+                      clearOnPress: () {}),
+                  Expanded(child: TicketListBody(ticketListDatum: data))
+                ]);
+              } else if (state is TicketsNotFetched) {
+                return const Center(
+                    child: Text(StringConstants.kNoRecordsFound));
+              }
+              return const SizedBox.shrink();
+            })));
+  }
+}
