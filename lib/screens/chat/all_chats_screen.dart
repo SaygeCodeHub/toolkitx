f4ee5adaec9d59eaa42e325d513bd9f6180cc4da
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2024-05-14 11:49:25 +0530

    TXAD-382-1: Reformat code.

--- lib/screens/chat/all_chats_screen.dart
+++ lib/screens/chat/all_chats_screen.dart
@@ -43,124 +43,127 @@ class AllChatsScreen extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return Padding(
-              padding: const EdgeInsets.symmetric(
-                  vertical: leftRightMargin, horizontal: leftRightMargin),
-              child: ListView.separated(
-                itemCount: snapshot.data!.length,
-                physics: const BouncingScrollPhysics(),
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return CustomCard(
-                    child: ListTile(
-                        onTap: () async {
-                          await databaseHelper.updateShowCountForMessages(
-                              snapshot.data![index].rId,
-                              snapshot.data![index].sId);
-                          context.read<ChatBloc>().chatDetailsMap = {
-                            "employee_name": snapshot.data![index].userName,
-                            'rid': (snapshot.data![index].isGroup == true)
-                                ? snapshot.data![index].groupId
-                                : snapshot.data![index].rId,
-                            'sid': snapshot.data![index].sId,
-                            'isReceiver': snapshot.data![index].isReceiver,
-                            'stype': snapshot.data![index].sType,
-                            'rtype': snapshot.data![index].rType,
-                            'isGroup': snapshot.data![index].isGroup
-                          };
-                          Navigator.pushNamed(
-                              context, ChatMessagingScreen.routeName);
-                        },
-                        leading: Container(
-                            padding: const EdgeInsets.all(tiniestSpacing),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15),
-                                color: AppColor.deepBlue),
-                            child: Icon(
-                                (snapshot.data![index].isGroup == true)
-                                    ? Icons.people
-                                    : Icons.person,
-                                color: AppColor.ghostWhite,
-                                size: 20)),
-                        title: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text((snapshot.data![index].isGroup == true)
-                                ? snapshot.data![index].groupName
-                                : snapshot.data![index].userName),
-                            Text(snapshot.data![index].date)
-                          ],
-                        ),
-                        titleTextStyle: Theme.of(context)
-                            .textTheme
-                            .small
-                            .copyWith(
-                                color: AppColor.black,
-                                fontWeight: FontWeight.w500),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const SizedBox(height: xxTiniestSpacing),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text((snapshot.data![index].isGroup == true)
-                                    ? snapshot.data![index].groupPurpose
-                                    : (snapshot.data![index].sType == '2')
-                                        ? 'Workforce'
-                                        : 'System User'),
-                                Text(snapshot.data![index].time,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .xxSmall
-                                        .copyWith(
-                                            color: AppColor.black,
-                                            fontWeight: FontWeight.w500))
-                              ],
-                            ),
-                            const SizedBox(height: xxTiniestSpacing),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Flexible(
-                                    child: messageText(
-                                        snapshot.data![index].message,
-                                        snapshot.data![index].messageType)),
-                                Visibility(
-                                  visible:
-                                      snapshot.data![index].unreadMsgCount != 0,
-                                  child: Container(
-                                      height: 15,
-                                      width: 15,
-                                      decoration: BoxDecoration(
-                                          color: AppColor.green,
-                                          borderRadius:
-                                              BorderRadius.circular(10)),
-                                      child: Center(
-                                          child: Text(
-                                              snapshot
-                                                  .data![index].unreadMsgCount
-                                                  .toString(),
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .xxSmall
-                                                  .copyWith(
-                                                      color: AppColor.black,
-                                                      fontWeight:
-                                                          FontWeight.w500)))),
-                                )
-                              ],
-                            ),
-                            const SizedBox(height: xxTiniestSpacing),
-                          ],
-                        ),
-                        subtitleTextStyle: Theme.of(context).textTheme.xSmall),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const SizedBox(height: xxTinierSpacing);
-                },
-              ),
-            );
+                padding: const EdgeInsets.symmetric(
+                    vertical: leftRightMargin, horizontal: leftRightMargin),
+                child: ListView.separated(
+                  itemCount: snapshot.data!.length,
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return CustomCard(
+                      child: ListTile(
+                          onTap: () async {
+                            await databaseHelper.updateShowCountForMessages(
+                                snapshot.data![index].rId,
+                                snapshot.data![index].sId);
+                            context.read<ChatBloc>().chatDetailsMap = {
+                              "employee_name": snapshot.data![index].userName,
+                              'rid': (snapshot.data![index].isGroup == true)
+                                  ? snapshot.data![index].groupId
+                                  : snapshot.data![index].rId,
+                              'sid': snapshot.data![index].sId,
+                              'isReceiver': snapshot.data![index].isReceiver,
+                              'stype': snapshot.data![index].sType,
+                              'rtype': snapshot.data![index].rType,
+                              'isGroup': snapshot.data![index].isGroup
+                            };
+                            Navigator.pushNamed(
+                                context, ChatMessagingScreen.routeName);
+                          },
+                          leading: Container(
+                              padding: const EdgeInsets.all(tiniestSpacing),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: AppColor.deepBlue),
+                              child: Icon(
+                                  (snapshot.data![index].isGroup == true)
+                                      ? Icons.people
+                                      : Icons.person,
+                                  color: AppColor.ghostWhite,
+                                  size: 20)),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text((snapshot.data![index].isGroup == true)
+                                  ? snapshot.data![index].groupName
+                                  : snapshot.data![index].userName),
+                              Text(snapshot.data![index].date)
+                            ],
+                          ),
+                          titleTextStyle: Theme.of(context)
+                              .textTheme
+                              .small
+                              .copyWith(
+                                  color: AppColor.black,
+                                  fontWeight: FontWeight.w500),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: xxTiniestSpacing),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text((snapshot.data![index].isGroup == true)
+                                      ? snapshot.data![index].groupPurpose
+                                      : (snapshot.data![index].sType == '2')
+                                          ? 'Workforce'
+                                          : 'System User'),
+                                  Text(snapshot.data![index].time,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .xxSmall
+                                          .copyWith(
+                                              color: AppColor.black,
+                                              fontWeight: FontWeight.w500))
+                                ],
+                              ),
+                              const SizedBox(height: xxTiniestSpacing),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                      child: messageText(
+                                          snapshot.data![index].message,
+                                          snapshot.data![index].messageType)),
+                                  Visibility(
+                                    visible:
+                                        snapshot.data![index].unreadMsgCount !=
+                                            0,
+                                    child: Container(
+                                        height: 15,
+                                        width: 15,
+                                        decoration: BoxDecoration(
+                                            color: AppColor.green,
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: Center(
+                                            child: Text(
+                                                snapshot
+                                                    .data![index].unreadMsgCount
+                                                    .toString(),
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .xxSmall
+                                                    .copyWith(
+                                                        color: AppColor.black,
+                                                        fontWeight:
+                                                            FontWeight.w500)))),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: xxTiniestSpacing),
+                            ],
+                          ),
+                          subtitleTextStyle:
+                              Theme.of(context).textTheme.xSmall),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(height: xxTinierSpacing);
+                  },
+                ));
           } else {
             return const SizedBox.shrink();
           }
