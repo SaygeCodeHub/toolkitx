092989fd1ef673aac565330959906b22583cad34
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2024-05-15 13:11:01 +0530

    TXAD-428: Implement configuration in build gradle for background check.

--- lib/blocs/wifiConnectivity/wifi_connectivity_bloc.dart
+++ lib/blocs/wifiConnectivity/wifi_connectivity_bloc.dart
@@ -46,8 +46,8 @@ class WifiConnectivityBloc
 
     // Check if location service is enabled
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    print('service enabled $serviceEnabled');
     if (!serviceEnabled) {
-      // Location service is not enabled, handle accordingly
       return Future.error('Location service disabled');
     }
 
@@ -55,27 +55,15 @@ class WifiConnectivityBloc
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
+      if (permission == LocationPermission.deniedForever) {
+        permission = await Geolocator.requestPermission();
         return Future.error('Location permissions are denied');
       }
     }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
     // Get current position
     return Geolocator.getCurrentPosition();
   }
 
-  // Fetch location
   void fetchLocation() async {
     try {
       Position position = await Geolocator.getCurrentPosition();
