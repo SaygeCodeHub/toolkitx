14747dec8838dae459acbb4390bab11a5e48f9c0
nikhilagashe <nikhilagashe31@gmail.com>
2024-05-15 20:20:40 +0530

    TXAD-430 : Fetch Message Model Updated

--- lib/data/models/notification/fetch_messages_model.dart
+++ lib/data/models/notification/fetch_messages_model.dart
@@ -1,10 +1,9 @@
+
 import 'dart:convert';
 
-FetchMessagesModel fetchMessagesModelFromJson(String str) =>
-    FetchMessagesModel.fromJson(json.decode(str));
+FetchMessagesModel fetchMessagesModelFromJson(String str) => FetchMessagesModel.fromJson(json.decode(str));
 
-String fetchMessagesModelToJson(FetchMessagesModel data) =>
-    json.encode(data.toJson());
+String fetchMessagesModelToJson(FetchMessagesModel data) => json.encode(data.toJson());
 
 class FetchMessagesModel {
   final int? status;
@@ -17,22 +16,17 @@ class FetchMessagesModel {
     this.data,
   });
 
-  factory FetchMessagesModel.fromJson(Map<String, dynamic> json) =>
-      FetchMessagesModel(
-        status: json["Status"],
-        message: json["Message"],
-        data: json["Data"] == null
-            ? []
-            : List<Datum>.from(json["Data"]!.map((x) => Datum.fromJson(x))),
-      );
+  factory FetchMessagesModel.fromJson(Map<String, dynamic> json) => FetchMessagesModel(
+    status: json["Status"],
+    message: json["Message"],
+    data: json["Data"] == null ? [] : List<Datum>.from(json["Data"]!.map((x) => Datum.fromJson(x))),
+  );
 
   Map<String, dynamic> toJson() => {
-        "Status": status,
-        "Message": message,
-        "Data": data == null
-            ? []
-            : List<dynamic>.from(data!.map((x) => x.toJson())),
-      };
+    "Status": status,
+    "Message": message,
+    "Data": data == null ? [] : List<dynamic>.from(data!.map((x) => x.toJson())),
+  };
 }
 
 class Datum {
@@ -41,7 +35,7 @@ class Datum {
   final String? processedDate;
   final int? userid;
   final int? usertype;
-  final Redirect? redirect;
+  final String? redirect;
   final String? redirectionkey;
   final DateTime? createddate;
   final int? isread;
@@ -61,51 +55,28 @@ class Datum {
   });
 
   factory Datum.fromJson(Map<String, dynamic> json) => Datum(
-        id: json["id"] ?? '',
-        notificationmessage: json["notificationmessage"] ?? '',
-        processedDate: json["processed_date"] ?? '',
-        userid: json["userid"] ?? '',
-        usertype: json["usertype"] ?? '',
-        redirect: redirectValues.map[json["redirect"] ?? ''],
-        redirectionkey: json["redirectionkey"] ?? '',
-        createddate: json["createddate"] == null
-            ? null
-            : DateTime.parse(json["createddate"]),
-        isread: json["isread"] ?? '',
-        processedDate1: json["processed_date1"] ?? '',
-      );
+    id: json["id"] ?? '',
+    notificationmessage: json["notificationmessage"] ?? '',
+    processedDate: json["processed_date"] ?? '',
+    userid: json["userid"] ?? '',
+    usertype: json["usertype"] ?? '',
+    redirect: json["redirect"] ?? '',
+    redirectionkey: json["redirectionkey"] ?? '',
+    createddate: json["createddate"] == null ? null : DateTime.parse(json["createddate"] ?? ''),
+    isread: json["isread"] ?? '',
+    processedDate1: json["processed_date1"] ?? '',
+  );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "notificationmessage": notificationmessage,
-        "processed_date": processedDate,
-        "userid": userid,
-        "usertype": usertype,
-        "redirect": redirectValues.reverse[redirect],
-        "redirectionkey": redirectionkey,
-        "createddate": createddate?.toIso8601String(),
-        "isread": isread,
-        "processed_date1": processedDate1,
-      };
-}
-
-enum Redirect { CERTIFICATECOURSE, EXPENSEREPORT, LOTO, WORKORDER }
-
-final redirectValues = EnumValues({
-  "certificatecourse": Redirect.CERTIFICATECOURSE,
-  "expensereport": Redirect.EXPENSEREPORT,
-  "loto": Redirect.LOTO,
-  "workorder": Redirect.WORKORDER
-});
-
-class EnumValues<T> {
-  Map<String, T> map;
-  late Map<T, String> reverseMap;
-
-  EnumValues(this.map);
-
-  Map<T, String> get reverse {
-    reverseMap = map.map((k, v) => MapEntry(v, k));
-    return reverseMap;
-  }
+    "id": id,
+    "notificationmessage": notificationmessage,
+    "processed_date": processedDate,
+    "userid": userid,
+    "usertype": usertype,
+    "redirect": redirect,
+    "redirectionkey": redirectionkey,
+    "createddate": createddate?.toIso8601String(),
+    "isread": isread,
+    "processed_date1": processedDate1,
+  };
 }
