f924fbc5168db6bd5a4da26db5be4647d0ef3ae0
nikhilagashe <nikhilagashe31@gmail.com>
2024-05-15 17:10:09 +0530

    TXAD-430 : Implement Get Message API in Notification Screen

--- lib/repositories/notification/notification_repository_impl.dart
+++ lib/repositories/notification/notification_repository_impl.dart
@@ -5,12 +5,11 @@ import '../../utils/dio_client.dart';
 import 'notification_repository.dart';
 
 class NotificationRepositoryImpl extends NotificationRepository {
-
   @override
-  Future<FetchMessagesModel> fetchMessages(String hashCode, String userId) async {
+  Future<FetchMessagesModel> fetchMessages(
+      String hashCode, String userId) async {
     final response = await DioClient().get(
         "${ApiConstants.baseUrl}common/getmessages?hashcode=$hashCode&userid=$userId");
     return FetchMessagesModel.fromJson(response);
   }
-
 }
