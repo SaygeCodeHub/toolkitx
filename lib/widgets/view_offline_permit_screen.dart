72bdc1cc0049c6ec0b0d0e210a93a0e484298f35
Aditya <adityaranaworks@gmail.com>
2024-05-17 23:07:55 +0530

    TXAD-382-1 : Fix cache issue

--- lib/widgets/view_offline_permit_screen.dart
+++ lib/widgets/view_offline_permit_screen.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:toolkit/utils/constants/html_constants.dart';
+import 'package:toolkit/widgets/text_button.dart';
 
 class OfflineHtmlViewerScreen extends StatelessWidget {
   const OfflineHtmlViewerScreen({super.key});
@@ -11,7 +15,16 @@ class OfflineHtmlViewerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          actions: [
+            CustomTextButton(
+              onPressed: () {
+                // generatePdfFromHtml(HTMLOfflineConstants.offlinePermitType12);
+              },
+              textValue: 'Generate PDF',
+            )
+          ],
+        ),
         body: InAppWebView(
           initialUrlRequest: URLRequest(
               url: WebUri.uri(Uri.dataFromString(
@@ -21,5 +34,24 @@ class OfflineHtmlViewerScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> generatePdfFromHtml(String htmlContent) async {
+    try {
+      Directory appDocDir = await getTemporaryDirectory();
+      String appDocPath = appDocDir.path;
+
+      // Define the output file path
+      var targetFileName = "example.pdf";
+      var generatedPdfFile = await FlutterHtmlToPdf.convertFromHtmlContent(
+        htmlContent,
+        appDocPath,
+        targetFileName,
+      );
+
+      print("Generated PDF File Path: ${generatedPdfFile.path}");
+    } catch (e) {
+      print("eeeeeee $e");
+    }
   }
 }
