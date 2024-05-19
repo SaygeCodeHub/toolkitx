d66f2382a6644a16d06d455c7a2fa696e7465256
Aditya <adityaranaworks@gmail.com>
2024-05-19 11:21:14 +0530

    TXAD-00 : Prepare iOS build

--- lib/screens/location/current_location_screen.dart
+++ lib/screens/location/current_location_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:geolocator/geolocator.dart';
 
 class CurrentLocationScreen extends StatefulWidget {
   const CurrentLocationScreen({super.key});
@@ -9,57 +10,99 @@ class CurrentLocationScreen extends StatefulWidget {
 }
 
 class _CurrentLocationScreenState extends State<CurrentLocationScreen> {
+  bool fetchingLocation = true;
   MapController mapController = MapController(
-    initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
-    areaLimit: BoundingBox(
-      east: 10.4922941,
-      north: 47.8084648,
-      south: 45.817995,
-      west: 5.9559113,
-    ),
-  );
+      initPosition: GeoPoint(latitude: 21.145800, longitude: 79.088158));
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return;
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    GeoPoint currentLocation =
+        GeoPoint(latitude: position.latitude, longitude: position.longitude);
+    mapController.changeLocation(currentLocation);
+    setState(() {
+      fetchingLocation = false;
+    });
+    mapController.addMarker(currentLocation,
+        markerIcon: const MarkerIcon(
+          icon: Icon(
+            Icons.location_on,
+            color: Colors.blue,
+            size: 48,
+          ),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return OSMFlutter(
-        controller: mapController,
-        osmOption: OSMOption(
-          userTrackingOption: const UserTrackingOption(
-            enableTracking: true,
-            unFollowUser: false,
-          ),
-          zoomOption: const ZoomOption(
-            initZoom: 8,
-            minZoomLevel: 3,
-            maxZoomLevel: 19,
-            stepZoom: 1.0,
-          ),
-          userLocationMarker: UserLocationMaker(
-            personMarker: const MarkerIcon(
-              icon: Icon(
-                Icons.location_history_rounded,
-                color: Colors.red,
-                size: 48,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Current Location')),
+      body: OSMFlutter(
+          controller: mapController,
+          osmOption: OSMOption(
+            userTrackingOption: const UserTrackingOption(
+              enableTracking: true,
+              unFollowUser: false,
+            ),
+            zoomOption: const ZoomOption(
+              initZoom: 15,
+              minZoomLevel: 9,
+              maxZoomLevel: 19,
+              stepZoom: 1.0,
+            ),
+            userLocationMarker: UserLocationMaker(
+              personMarker: const MarkerIcon(
+                icon: Icon(
+                  Icons.location_history_rounded,
+                  color: Colors.red,
+                  size: 48,
+                ),
+              ),
+              directionArrowMarker: const MarkerIcon(
+                icon: Icon(
+                  Icons.double_arrow,
+                  size: 48,
+                ),
               ),
             ),
-            directionArrowMarker: const MarkerIcon(
+            roadConfiguration: const RoadOption(
+              roadColor: Colors.yellowAccent,
+            ),
+            markerOption: MarkerOption(
+                defaultMarker: const MarkerIcon(
               icon: Icon(
-                Icons.double_arrow,
-                size: 48,
+                Icons.person_pin_circle,
+                color: Colors.blue,
+                size: 56,
               ),
-            ),
-          ),
-          roadConfiguration: const RoadOption(
-            roadColor: Colors.yellowAccent,
-          ),
-          markerOption: MarkerOption(
-              defaultMarker: const MarkerIcon(
-            icon: Icon(
-              Icons.person_pin_circle,
-              color: Colors.blue,
-              size: 56,
-            ),
+            )),
           )),
-        ));
+    );
   }
 }
