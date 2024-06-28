8e003550242c2ad21b179a9b910c7282886ecf80
nikhilagashe <nikhilagashe31@gmail.com>
2024-06-28 12:07:36 +0530

    TXAD-450 : Replaced Popupmenu to Menuanchor(Horizontal menu) in Group Chat Screen and Group Details Screen.

--- lib/screens/chat/widgets/group_details_popup_menu.dart
+++ lib/screens/chat/widgets/group_details_popup_menu.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:toolkit/configs/app_theme.dart';
 
 class GroupDetailsPopupMenu extends StatelessWidget {
   const GroupDetailsPopupMenu({super.key});
 
-  PopupMenuItem _buildPopupMenuItem(context, String title, String position) {
-    return PopupMenuItem(
-        value: position,
-        child: Text(title, style: Theme.of(context).textTheme.xSmall));
-  }
-
   @override
   Widget build(BuildContext context) {
     List popUpMenuItems = ['Remove Member', 'Set as Admin', 'Dismiss as Admin'];
-    return PopupMenuButton(
-        onSelected: (value) {
-          if (value == 'Remove Member') {}
-        },
-        position: PopupMenuPosition.under,
-        itemBuilder: (BuildContext context) => [
-              for (int i = 0; i < popUpMenuItems.length; i++)
-                _buildPopupMenuItem(
-                    context, popUpMenuItems[i], popUpMenuItems[i])
-            ]);
+    return MenuAnchor(
+      builder:
+          (BuildContext context, MenuController controller, Widget? child) {
+        return IconButton(
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+          icon: const Icon(Icons.more_horiz),
+        );
+      },
+      alignmentOffset: const Offset(-80.0, 0.0),
+      menuChildren: List<MenuItemButton>.generate(
+        popUpMenuItems.length,
+        (int index) => MenuItemButton(
+          onPressed: () {
+            callFunction(popUpMenuItems[index], context);
+          },
+          child: Text(popUpMenuItems[index]),
+        ),
+      ),
+    );
+  }
+
+  callFunction(
+    buttonName,
+    context,
+  ) {
+    switch (buttonName) {
+      case "Remove Member":
+        print("Remove Member");
+        break;
+      case "Set as Admin":
+        print("Set as Admin");
+        break;
+      case "Dismiss as Admin":
+        print("Dismiss as Admin");
+        break;
+    }
   }
 }
